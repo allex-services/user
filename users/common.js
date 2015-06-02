6f2618aec0355b93593e39c4d1413b7acb24487d
Andrija Petrovic <andrija.hers@gmail.com>
2015-06-02 19:09:06 +0200

    And the missing file

---
+++ users/common.js
@@ -0,0 +1,20 @@
+function createUserCommons(execlib,klass){
+  var lib = execlib.lib;
+
+  function reverseSet = function(state,item,itemname){
+    state.set(itemname,item);
+  }
+  klass.prototype.fillState = function(prophash){
+    lib.traverse(prophash,reverseSet.bind(null,this.state));
+  }
+  klass.prototype.onAllSessionsDown = function(sessions){
+    lib.runNext(this.recheckSessions.bind(this,sessions),10000);
+  };
+  klass.prototype.recheckSessions = function(sessions){
+    if(sessions.length<1){
+      this.destroy();
+    }
+  };
+}
+
+module.exports = createUserCommons;
