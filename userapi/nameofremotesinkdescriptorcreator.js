bade5893810485fd4eae0b236e37bde9b2893c76
Veljko Popovic <veljko@888.rs>
2015-11-13 14:38:24 +0100

    fixing obsolete scaffolder code

--- userapi/nameofremotesinkdescriptorcreator.js
+++ userapi/nameofremotesinkdescriptorcreator.js
@@ -3,14 +3,14 @@ function createNameOfRemoteDescptorFunc(execlib) {
   var lib = execlib.lib;
   return function nameOfRemoteSinkDescriptor (sinkinfo) {
     if (lib.isArray(sinkinfo.name)) {
-      return sinkinfo.name[sinkinfo.name.length-1];
+      return sinkinfo.alias || sinkinfo.name[sinkinfo.name.length-1];
     }
     if (!sinkinfo.name) {
       console.error(sinkinfo);
       throw new lib.Error('NO_LOCAL_SUBSINK_NAME');
     }
 
-    return sinkinfo.name;
+    return sinkinfo.alias || sinkinfo.name;
   }
 }
 
