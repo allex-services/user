6e0afc6b88c9c8ee9fcc276552e39ce6a84efb0b
Andrija Petrovic <andrija.hers@gmail.com>
2017-02-24 12:53:55 +0000

    CsvFile is now capable of producing content from a DataService via produceFromDataArray

--- userapi/csvfilecreator.js
+++ userapi/csvfilecreator.js
@@ -10,6 +10,58 @@ function createCSVFile (execlib, DynamicFile) {
   CsvFile.prototype.contentType = function () {
     return 'text/csv';
   };
+  /*
+   * datafields: [{
+   *  name: 'field0',
+   *  title: 'Field0'
+   * },{
+   *  name: 'field1',
+   *  title: 'Field1'
+   * }]
+   */
+  CsvFile.prototype.headerName = function (field) {
+    return this.fieldContents(field.title || field.name);
+  };
+  CsvFile.prototype.headerNames = function (datafields) {
+    return datafields.map(this.headerName.bind(this)).join(this.fieldDelimiter);
+  };
+  CsvFile.prototype.produceFromDataArray = function (datafields, dataarray) {
+    var ret = this.includeHeaders ? this.headerNames(datafields)+'\n' : '';
+    console.log('ima li date?', dataarray);
+    return ret+dataarray.reduce(this.rowProducer.bind(this, datafields), '');
+  };
+  CsvFile.prototype.rowProducer = function (datafields, res, dataobject) {
+    console.log('sta je res kojmoj?', res);
+    var retobj = {ret: ''};
+    datafields.forEach(this.onDataField.bind(this, datafields, dataobject, retobj));
+    if (res.length) {
+      res += '\n';
+    }
+    res += retobj.ret;
+    retobj = null;
+    return res;
+  };
+  CsvFile.prototype.onDataField = function (datafields, dataobject, retobj, datafield, ind) {
+    retobj.ret += this.fieldProducer(dataobject,datafield.name);
+    if (ind < datafields.length-1) {
+      retobj.ret += this.fieldDelimiter;
+    }
+  };
+  CsvFile.prototype.fieldProducer = function (dataobject, fieldname) {
+    var ret = dataobject[fieldname];
+    if (!lib.isString(ret)) {
+      ret = '';
+    }
+    return this.fieldContents(ret);
+  };
+  CsvFile.prototype.fieldContents = function (string) {
+    if (string.indexOf(this.fieldDelimiter) >= 0) {
+      string = this.textDelimiter+string+this.textDelimiter;
+    }
+    return string;
+  };
+  CsvFile.prototype.fieldDelimiter = ',';
+  CsvFile.prototype.textDelimiter = '"';
 
   return CsvFile;
 }
